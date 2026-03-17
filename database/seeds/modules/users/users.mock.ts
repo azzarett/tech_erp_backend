2312d4e743ee2c7957a8ed84dd7d9e966a8dcfc3
Азат <zachet04@gmail.com>
2026-03-17 11:00:09 +0500

    feat(users): add mock user data with hashed password for testing

--- database/seeds/modules/users/users.mock.ts
+++ database/seeds/modules/users/users.mock.ts
@@ -1,1 +1,10 @@
-export const users = [];
+const hashedPassword =
+  '$2a$12$ow4ogWrtmtfUsHYugvWlCe.tz5OetmsmIG67a24J.1/tifjeGbph2'; // password
+
+export const users = [
+  {
+    id: '8bc18f6e-ae73-4c7a-9fe3-a85a6f8d4bd0',
+    identifier: 'user@example.com',
+    password: hashedPassword,
+  },
+];
